6150885532fd6b57a75c4f4302718c3285fdeb6f
Titop Sur <titopsur@gmail.com>
2013-07-10 12:27:24 +0200

    from fs 2

--- web.js
+++ web.js
@@ -4,11 +4,10 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  Buffer bf = new Buffer(fs.readFileSync('index.html'));
-  response.send(bf.toString());
-});
+response.send((new Buffer(fs.readFileSync('index.html',encoding='utf8')).toString()))});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
