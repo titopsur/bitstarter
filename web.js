1ca9926b8c545bcd3d77728b138b8fb4166c5e83
titopsur <titopsur@gmail.com>
2013-07-28 14:47:34 +0200

    test dev

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 response.send((new Buffer(fs.readFileSync('index.html',encoding='utf8')).toString()))});
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
